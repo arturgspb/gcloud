98001e0d881471f973a4b680790d507f11a26af9
arturgspb <art@realweb.ru>
2018-09-04 00:29:36 +0300

    init

---
+++ 01-create-kuber.sh
@@ -0,0 +1,8 @@
+gcloud config set project meta-test-164215
+
+gcloud config set compute/zone us-west1-a
+
+gcloud container clusters create test-01
+
+gcloud container clusters get-credentials test-01
+
---
+++ 02-start-app.sh
@@ -0,0 +1,14 @@
+# kubectl delete deployment my-app
+# kubectl delete service my-app-service
+# kubectl delete service my-app
+
+
+# kubectl delete deployment,ingress,service -l app=my-app
+# gcloud compute addresses delete my-app-ip --global
+
+gcloud compute addresses create my-app-ip --global
+gcloud compute addresses describe my-app-ip --global
+
+kubectl apply -f deployment_t2.yaml
+kubectl apply -f deployment_t2.yaml
+kubectl apply -f basic_ingress.yaml
