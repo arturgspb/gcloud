3c0cd9c5b330ce3078a3fbb2afa7c8a483a7323d
arturgspb <art@realweb.ru>
2018-09-04 20:21:27 +0300

    fix ingress

--- 03-tag-image.sh
+++ 03-tag-image.sh
@@ -2,3 +2,8 @@ docker tag e82b8f89a033 eu.gcr.io/meta-test-164215/qrcode--v1:latest
 
 # run "gcloud auth configure-docker" if need
 docker push eu.gcr.io/meta-test-164215/qrcode--v1:latest
+
+
+docker tag 12bc63994c13 eu.gcr.io/meta-test-164215/grpc-test--v1:v1.1
+
+docker push eu.gcr.io/meta-test-164215/grpc-test--v1:v1.1
